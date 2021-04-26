82f399c190443846a2b47971cc7f143d578f96e7
xiaobaowan1988 <baoliangwan1988@gmail.com>
2021-04-26 19:08:45 +0800

    fix v2 error

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <HelloWorld msg="Welcome to Your Vue.js App v2" />
 </template>
 
 <script>
@@ -9,7 +9,7 @@ import HelloWorld from './components/HelloWorld.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld V2
+    HelloWorld
   }
 }
 </script>
