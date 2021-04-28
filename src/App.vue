fae0959de1659e065d5474bf863d2e59606f184e
xiaobaowan1988 <55383125+xiaobaowan1988@users.noreply.github.com>
2021-04-28 12:12:00 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
     <h1>Todo App</h1>
     <input type="text" v-model="name" placeholder="Todo name">
     <input type="text" v-model="description" placeholder="Todo description">
-    <button v-on:click="createTodo">Create Todo v1024</button>
+    <button v-on:click="createTodo">Create Todo</button>
     <div v-for="item in todos" :key="item.id">
       <h3>{{ item.name }}</h3>
       <p>{{ item.description }}</p>
